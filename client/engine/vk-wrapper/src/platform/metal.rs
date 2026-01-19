3dffe52dd7064a0962041991bc3d3e2f8cf84482
volodya7292 <a7292969@gmail.com>
2026-01-20 00:18:46 +0200

    remove cocoa

--- client/engine/vk-wrapper/src/platform/metal.rs
+++ client/engine/vk-wrapper/src/platform/metal.rs
@@ -1,6 +1,6 @@
 use core_graphics::base::CGFloat;
-use objc::runtime::{Object, BOOL, YES};
-use objc::{class, msg_send, sel, sel_impl};
+use objc2::runtime::{Object, BOOL, YES};
+use objc2::{class, msg_send};
 use raw_window_handle::AppKitWindowHandle;
 use std::ffi::c_void;
 use std::mem;
@@ -24,7 +24,7 @@ pub unsafe fn metal_layer_from_handle(handle: AppKitWindowHandle) -> Layer {
 }
 
 pub unsafe fn metal_layer_from_ns_view(view: *mut c_void) -> Layer {
-    let view: cocoa::base::id = mem::transmute(view);
+    let view: *mut Object = mem::transmute(view);
 
     // Check if the view is a CAMetalLayer
     let class = class!(CAMetalLayer);
@@ -50,7 +50,7 @@ pub unsafe fn metal_layer_from_ns_view(view: *mut c_void) -> Layer {
         let () = msg_send![view, setLayer: layer];
         let () = msg_send![view, setWantsLayer: YES];
 
-        let window: cocoa::base::id = msg_send![view, window];
+        let window: *mut Object = msg_send![view, window];
         if !window.is_null() {
             let scale_factor: CGFloat = msg_send![window, backingScaleFactor];
             let () = msg_send![layer, setContentsScale: scale_factor];
@@ -59,7 +59,7 @@ pub unsafe fn metal_layer_from_ns_view(view: *mut c_void) -> Layer {
         Layer::Allocated(layer)
     };
 
-    let _: *mut c_void = msg_send![view, retain];
+    let _: *mut Object = msg_send![view, retain];
     render_layer
 }
 
@@ -71,11 +71,11 @@ pub unsafe fn metal_layer_from_ns_window(window: *mut c_void) -> Layer {
 
 pub unsafe fn metal_layer_update(handle: AppKitWindowHandle) {
     let contents_scale: CGFloat = if !handle.ns_view.is_null() {
-        let view: cocoa::base::id = mem::transmute(handle.ns_view);
+        let view: *mut Object = mem::transmute(handle.ns_view);
         msg_send![view, backingScaleFactor]
     } else if !handle.ns_window.is_null() {
-        let window: cocoa::base::id = mem::transmute(handle.ns_window);
-        let ns_view: cocoa::base::id = msg_send![window, contentView];
+        let window: *mut Object = mem::transmute(handle.ns_window);
+        let ns_view: *mut Object = msg_send![window, contentView];
         msg_send![ns_view, backingScaleFactor]
     } else {
         panic!("Invalid appkit handle");
